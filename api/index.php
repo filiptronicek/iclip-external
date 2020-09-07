9782cc2a9b226ae98cde46bcccdb79ca2c8feedd
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-07 17:49:25 +0200

    Add some content types

--- api/index.php
+++ api/index.php
@@ -7,8 +7,46 @@ $data = file_get_contents($url);
 $ext = explode(".", $url);
 $ext = end($ext);
 
-if ($ext != "png" | $ext != "jpg") {
-    $ext = "png";
+$headerContent = "";
+
+switch ($ext) {
+
+    /* Application */
+
+    case "pdf":
+        $headerContent = "application/pdf";
+        break;
+
+    case "js":
+        $headerContent = "application/javascript";
+        break;
+
+    case "ogg":
+        $headerContent = "application/ogg";
+        break;  
+
+    case "json":
+        $headerContent = "application/json";
+        break;
+
+    /* Text */
+
+    case "css":
+        $headerContent = "text/css";
+        break;
+
+    case "html":
+        $headerContent = "text/html";
+        break;
+
+    /* Image */
+
+    case "png":
+        $headerContent = "image/png";
+    
+    case "jpg":
+        $headerContent = "image/jpeg";
+    
 }
 
 header("Content-type: image/{$ext}");
